4a1c278c51ae4bfa24c234e9866614881ec8cda9
wu-tzu-hsuan <cherry51120@gmail.com>
2019-09-09 15:42:52 +0800

    header 大阪

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2019, Ginkgo
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+ 
+$(function() {
+    var stageItems = $('.stage-slides .stage-item').length - 1;
+    var currentStage = 0;
+    setInterval(function() {
+        if (currentStage > stageItems)
+            currentStage = 0;
+
+        $('.stage-slides .stage-item').eq(currentStage).addClass('active').siblings().removeClass('active');
+        $('.circle-spot > li').eq(currentStage).addClass('active').siblings().removeClass('active');
+        currentStage++;
+    }, 5000);
+
+    $(window).on('scroll', function () {
+        let scrollPosition = window.scrollY;
+
+        if (scrollPosition > 200) {
+            $('.navigate').addClass('active');
+        } else {
+            $('.navigate').removeClass('active');
+        }
+    });
+});
